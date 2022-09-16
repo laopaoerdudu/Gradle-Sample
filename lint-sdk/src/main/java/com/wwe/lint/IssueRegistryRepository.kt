48b65014770c1fbf1246425a9fdf5a600d7f34da
David B ZHANG <18588744489@163.com>
2022-09-16 09:49:09 +0800

    Update

--- lint-sdk/src/main/java/com/wwe/lint/IssueRegistryRepository.kt
+++ lint-sdk/src/main/java/com/wwe/lint/IssueRegistryRepository.kt
@@ -1,6 +1,7 @@
 package com.wwe.lint
 
 import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.detector.api.Issue
 import com.android.tools.lint.detector.api.CURRENT_API
 
@@ -17,11 +18,11 @@ class IssueRegistryRepository : IssueRegistry() {
     override val minApi: Int
         get() = 8 // Works with Studio 4.1 or later; see com.android.tools.lint.detector.api.Api / ApiKt
 
-    // Requires lint API 30.0+; if you're still building for something
-    // older, just remove this property.
-//    override val vendor: Vendor = Vendor(
-//        vendorName = "Android Open Source Project",
-//        feedbackUrl = "https://github.com/googlesamples/android-custom-lint-rules/issues",
-//        contact = "https://github.com/googlesamples/android-custom-lint-rules"
-//    )
+//     Requires lint API 30.0+; if you're still building for something
+//     older, just remove this property.
+    override val vendor: Vendor = Vendor(
+        vendorName = "Android Open Source Project",
+        feedbackUrl = "https://github.com/laopaoerdudu/Gradle-Sample/issues",
+        contact = "https://github.com/laopaoerdudu/Gradle-Sample"
+    )
 }
